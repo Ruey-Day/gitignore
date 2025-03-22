129ef3b2627a4e80ff2ba4e271969bfb5573dc06
Ruey-Day <rueyday1@gmail.com>
2025-03-22 17:30:07 -0400

    added common ignore file

--- load_model.py
+++
@@ -1,6 +0,0 @@
-from transformers import LlamaForCausalLM, LlamaTokenizer
-import torch
-
-# Load the tokenizer and model
-tokenizer = LlamaTokenizer.from_pretrained("path_to_llama_model")
-model = LlamaForCausalLM.from_pretrained("path_to_llama_model").to("cuda")
